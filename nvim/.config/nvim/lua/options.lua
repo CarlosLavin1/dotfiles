7d96f75d7e2d633b61ff05c2b96a53bc44565f59
CarlosLavin1 <carlosfranciscolavin@gmail.com>
2025-03-14 23:20:29 -0300

    added git integration

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,13 +1,18 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.smartindent = true
 vim.opt.wrap = false
 vim.opt.termguicolors = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 50
+
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_winsize = 25
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
       local builtin = require("telescope.builtin")
 
       vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<C-g>', builtin.git_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     end
   },
