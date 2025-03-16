d0a71985f78e02cdf19fb058da7abba9028dd9da
CarlosLavin1 <carlosfranciscolavin@gmail.com>
2025-03-15 23:26:31 -0300

    removed neo-tree

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,12 +1,12 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", 
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    vim.keymap.set('n', '<C-f>', ':Neotree source=filesystem reveal=true action=show position=left toggle=true<CR>', {})
-  end
+--  "nvim-neo-tree/neo-tree.nvim",
+  --branch = "v3.x",
+  --dependencies = {
+    --"nvim-lua/plenary.nvim",
+    --"nvim-tree/nvim-web-devicons", 
+    --"MunifTanjim/nui.nvim",
+  --},
+  --config = function()
+    --vim.keymap.set('n', '<leader>ft', ':Neotree source=filesystem reveal=true action=show position=left toggle=true<CR>', {})
+  --end
 }
